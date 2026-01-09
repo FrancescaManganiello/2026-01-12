d6e0de3504b4c43e42a4fa370c1a74fb1a35198e
Giuseppe Averta <g.averta3@gmail.com>
2026-01-09 17:38:29 +0100

    first commit

---
+++ model/Constructor.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+import datetime
+
+@dataclass
+class Constructor:
+    constructorId: int
+    constructorRef: str
+    name: str
+    nationality: str
+    oldest_driver_dob: datetime.date = None
+
+    def __hash__(self):
+        return hash(self.constructorId)
+
+    def __str__(self):
+        return f"{self.constructorRef} ({self.name})"
